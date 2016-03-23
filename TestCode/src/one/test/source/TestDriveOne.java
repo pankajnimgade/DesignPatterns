463488195c442da90d87f294fe1663bf18b740f3
pankajnimgade <pankaj.nimgade@gmail.com>
2016-03-24 00:45:42 +0530

    first commit

---
+++ TestCode/src/one/test/source/TestDriveOne.java
@@ -0,0 +1,11 @@
+package one.test.source;
+
+/**
+ * Created by Pankaj Nimgade on 24-03-2016.
+ */
+public class TestDriveOne {
+
+    public static void main(String[] args){
+        System.out.println("test ");
+    }
+}
---
+++ src/design/pattern/test/one/Main.java
@@ -0,0 +1,8 @@
+package design.pattern.test.one;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
