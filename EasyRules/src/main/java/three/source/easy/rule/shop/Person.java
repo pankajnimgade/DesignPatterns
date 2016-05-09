22b2befebb527bcc334f87546ecf599fc15a3dea
pankajnimgade <pankaj.nimgade@gmail.com>
2016-05-09 10:13:17 +0530

    easy rules shop example

---
+++ EasyRules/src/main/java/three/source/easy/rule/shop/Person.java
@@ -0,0 +1,42 @@
+package three.source.easy.rule.shop;
+
+/**
+ * Created by Pankaj Nimgade on 09-05-2016.
+ */
+public class Person {
+
+    private String name;
+
+    private int age;
+
+    private boolean isAdult;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isAdult() {
+        return isAdult;
+    }
+
+    public void setAdult(boolean adult) {
+        isAdult = adult;
+    }
+}
---
+++ EasyRules/src/main/java/three/source/easy/rule/shop/ReadMe.md
@@ -0,0 +1,4 @@
+In this tutorial, we have a shop application and we would like to implement the following requirement: deny children from buying alcohol. The minimal legal age to be considered as adult is 18. This tutorial is split in two parts:
+
+    Part 1: Implement the business rule of denying children from buying alcohol
+    Part 2: Make the legal age configurable via JMX so we can change it at runtime
