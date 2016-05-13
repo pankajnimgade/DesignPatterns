cc05a40c410264c83018652c35d0604c48c2384c
pankajnimgade <pankaj.nimgade@gmail.com>
2016-05-14 00:29:41 +0530

    add all untraced files to project

---
+++ Singleton/src/main/java/singleton/test/source/App.java
@@ -0,0 +1,13 @@
+package singleton.test.source;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ TemplateMethodPattern/src/main/java/template/method/pattern/App.java
@@ -0,0 +1,13 @@
+package template.method.pattern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
